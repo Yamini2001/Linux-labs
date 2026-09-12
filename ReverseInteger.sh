89d4bd0d6582b1b6a9e499ff0f89720a708072f2
BeSingularTeam <besingulardemo@gmail.com>
2026-09-12 23:22:21 +0530

    ReverseIntger

--- ReverseInteger.sh
+++ ReverseInteger.sh
@@ -1,17 +1,28 @@
 #Calculate the reverse integer
 #!/bin/bash
 
-read -p "Enter a number to reverse: " num
+#!/bin/bash
+
+read -p "Enter a number: " num
+
+
+original_num=$num
 
 reverse=0
+sum_of_digits=0
 
-while (( num > 0 )); do
+while (( num > 0 )); 
+do
     digit=$(( num % 10 ))
     
     
     reverse=$(( (reverse * 10) + digit ))
-    
+    sum_of_digits=$(( sum_of_digits + digit ))
     num=$(( num / 10 ))
 done
 
-echo "Reversed Number: $reverse"
+echo "-----------------------------------"
+echo "Original Number : $original_num"
+echo "Reversed Number : $reverse"
+echo "Sum of Digits   : $sum_of_digits"
+echo "-----------------------------------"
