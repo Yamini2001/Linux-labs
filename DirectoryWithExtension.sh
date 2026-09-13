9502deae3a449a260ac36820d686879451674dd0
BeSingularTeam <besingulardemo@gmail.com>
2026-09-13 17:19:30 +0530

    commit

---
+++ DirectoryWithExtension.sh
@@ -0,0 +1,11 @@
+read -p "Enter the directory name: " dir_name
+read -p "Enter a file extension: " extension
+file=$(find "$directory" -type f -name "*.$extension")
+if [ -z "$file" ];
+then
+  echo "No .$extension files found."
+else
+  echo $file
+  count=$(echo "$files" | wc -l)
+  echo "Total .$extension files = $count"
+fi
